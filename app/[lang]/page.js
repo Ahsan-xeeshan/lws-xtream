fdcd434fed6d081d28f0c8c6a01d59947f2e4d89
Xeeshan <xeeshann.ahsan@gmail.com>
2024-12-09 22:28:15 +0600

    api setting

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,11 +1,9 @@
 import VideoList from "@/components/VideoList";
+import { videos } from "@/lib/video-data";
 import { getDictionary } from "./dictionaries";
 
 export default async function Home({ params: { lang } }) {
   const dictionary = await getDictionary(lang);
-
-  const response = await fetch(`${process.env.BASE_API_URL}/videos`);
-  const videos = await response.json();
 
   return <VideoList videos={videos} dictionary={dictionary} />;
 }
